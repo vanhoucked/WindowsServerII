331f89ffd1d5eca8a4df46284392df50c626e147
Dré Vanhoucke <info@vanhouckedre.be>
2022-11-23 23:48:20 +0100

    afwerking vboxmanage

--- script.ps1
+++ script.ps1
@@ -14,6 +14,7 @@ $processorTable = @{$AD=2;$CA=1;$IIS=1;$Exchange=4;$Client=2}
 
 $vmArray = $AD,$CA,$IIS,$Exchange,$Client
 $serverArray = $AD,$CA,$IIS,$Exchange
+$functieTable = @{$AD="AD";$CA="CA";$IIS="IIS";$Exchange="Exchange";$Client="Client"}
 
 #VM's aanmaken
 foreach ($vm in $serverArray) {
@@ -32,14 +33,36 @@ try {
     Write-Output "[CREATIE MELDING] Er gebeurde een fout tijdens het aanmaken van $Client."
 }
 
+foreach ($vm in $vmArray) {
+    try {
+        VBoxManage modifyvm $vm --graphicscontroller=vboxsvga --vram=16
+        Write-Output "[CREATIE MELDING] Er werd 16MB videogeheugen toegekend aan $vm."
+    } catch {
+        Write-Output "[CREATIE MELDING] Er gebeurde een fout tijdens het toekennen van videogeheugen aan $vm."
+    }
+}
+try {
+  VBoxManage modifyvm $AD --graphicscontroller=vboxsvga --vram=32 
+  Write-Output "[CREATIE MELDING] Er werd 32MB videogeheugen toegekend aan $AD."
+} catch {
+    Write-Output "[CREATIE MELDING] Er gebeurde een fout tijdens het toekennen van videogeheugen aan $AD."
+}
+try {
+    VBoxManage modifyvm $Client --graphicscontroller=vboxsvga --vram=32 
+    Write-Output "[CREATIE MELDING] Er werd 32MB videogeheugen toegekend aan $Client."
+  } catch {
+      Write-Output "[CREATIE MELDING] Er gebeurde een fout tijdens het toekennen van videogeheugen aan $Client."
+  }
+
+VBoxManage modifyvm $Client --graphicscontroller=vboxsvga --vram=32
 
 #Virtuele opslagapparaten aanmaken
 foreach ($vm in $vmArray) {
     try {
         $grootteMB = $opslagTable.$vm * 1024
-        VBoxManage createmedium --filename=/vdi/$vm.vdi --size=$grootteMB
+        VBoxManage createmedium --filename=vdi/$vm.vdi --size=$grootteMB
         VBoxManage storagectl $vm --name="SATA Controller $vm" --add sata --controller=IntelAHCI
-        VBoxManage storageattach $vm --storagectl="SATA Controller $vm" --port=0 --type hdd --medium=/vdi/$vm.vdi
+        VBoxManage storageattach $vm --storagectl="SATA Controller $vm" --port=0 --device=0 --type hdd --medium=vdi/$vm.vdi
         Write-Output "[OPSLAG MELDING] Virtuele harde schijf aangemaakt voor $vm met een grootte van $grootteMB MB." 
     } catch {
         Write-Output "[OPSLAG MELDING] Er gebeurde een fout tijdens het aanmaken van de harde schijf voor $vm."
@@ -50,7 +73,7 @@ foreach ($vm in $vmArray) {
 foreach ($vm in $serverArray) {
     try {
         VBoxManage storagectl $vm --name="IDE Controller $vm" --add ide
-        VBoxManage storageattach $vm --storagectl="IDE Controller $vm" --port=1 --type dvddrive --medium=/iso/Windows2019.iso
+        VBoxManage storageattach $vm --storagectl="IDE Controller $vm" --port=1 --device=0 --type dvddrive --medium=iso/Windows2019.iso
         Write-Output "[OPSLAG MELDING] ISO gemount aan $vm."
     } catch {
         Write-Output "[OPSLAG MELDING] Er gebeurde een fout tijdens het mounten van de iso aan $vm."
@@ -58,7 +81,7 @@ foreach ($vm in $serverArray) {
 }
 try {
     VBoxManage storagectl $Client --name="IDE Controller $Client" --add ide
-    VBoxManage storageattach $Client --storagectl="IDE Controller $Client" --port=1 --type dvddrive --medium=/iso/Windows10.iso
+    VBoxManage storageattach $Client --storagectl="IDE Controller $Client" --port=1 --device=0 --type dvddrive --medium=iso/Windows10.iso
     Write-Output "[OPSLAG MELDING] ISO gemount aan $Client."
 } catch {
     Write-Output "[OPSLAG MELDING] Er gebeurde een fout tijdens het mounten van de iso aan $Client."
@@ -78,8 +101,9 @@ foreach ($vm in $vmArray) {
 #vCPU cores toekennen
 foreach ($vm in $vmArray) {
     try {
-        VBoxManage modifyvm $vm --cpus $processorTable.$vm
-        Write-Output "[CPU MELDING] $processorTable.$vm cores toegekend aan $vm."
+        $aantalCores = $processorTable.$vm
+        VBoxManage modifyvm $vm --cpus $aantalCores
+        Write-Output "[CPU MELDING] $aantalCores cores toegekend aan $vm."
     } catch {
         Write-Output "[CPU MELDING] Er gebeurde een fout tijdens het toekennen van de processorkernen aan $vm."
     }
@@ -104,9 +128,13 @@ try {
 #gedeelde map met powershell scripts
 foreach ($vm in $vmArray) {
     try {
-        VBoxManage sharedfolder add $vm --name=scripts --hostpath=/scripts --readonly --automount
+        $functie = $functieTable.$vm
+        VBoxManage sharedfolder add $vm --name=scripts --hostpath=scripts/$functie --readonly --automount
         Write-Output "[OPSLAG MELDING] De gedeelde folder met powershell scripts werd gemount aan $vm."
     } catch {
         Write-Output "[OPSLAG MELDING] De gedeelde folder met powershell scripts kon niet gemount worden aan $vm."
     }
 }
+
+#VM starten
+VBoxManage startvm $AD
