ad2b657ee6fb372a0e83490759b153218f0efe5b
Dré Vanhoucke <info@vanhouckedre.be>
2022-12-01 20:20:00 +0100

    user automatisch maken via csv bestand

--- scripts/AD/addUsers.ps1
+++ scripts/AD/addUsers.ps1
@@ -1,14 +1,16 @@
 $ouArray = "Management", "IT", "Accounting", "HR", "Sales"
 
-foreach ($ou in $ouArray) {
-    New-ADOrganizationalUnit -Name "$ou" -Path "DC=WS2-2223-Dre,DC=hogent" –ProtectedFromAccidentalDeletion "$False"
+Foreach ($groep in $ouArray) {
+    New-ADOrganizationalUnit -Name $groep -Path "DC=WS2-2223-Dre,DC=hogent" 
+    Set-ADOrganizationalUnit -Identity "OU=$groep,DC=WS2-2223-Dre,DC=hogent" -ProtectedFromAccidentalDeletion $False
     #protected geeft probleem
 }
 
-$securePassword = ConvertTo-SecureString -String "HoGent2022" -AsPlainText -Force
-
-New-ADUser -Name "Peter Griffin" -SamAccountName "pgriffin" -Path "OU=Management,DC=WS2-2223-Dre,DC=hogent" -Accountpassword $securePassword –Enabled $True
-New-ADUser -Name "Stewie Griffin" -SamAccountName "sgriffin" -Path "OU=IT,DC=WS2-2223-Dre,DC=hogent" -Accountpassword $securePassword –Enabled $True
-New-ADUser -Name "Joe Swanson" -SamAccountName "jswanson" -Path "OU=Accounting,DC=WS2-2223-Dre,DC=hogent" -Accountpassword $securePassword –Enabled $True
-New-ADUser -Name "Glenn Quagmire" -SamAccountName "gquagmire" -Path "OU=Accounting,DC=WS2-2223-Dre,DC=hogent" -Accountpassword $securePassword –Enabled $True
-New-ADUser -Name "Lois Griffin" -SamAccountName "lgriffin" -Path "OU=Management,DC=WS2-2223-Dre,DC=hogent" -Accountpassword $securePassword –Enabled $True
+$Import = Import-CSV "users.csv"
+$BasicOU = "DC=WS2-2223-Dre,DC=hogent"
+ 
+Foreach ($user in $Import) {
+    $password = $user.Wachtwoord | ConvertTo-SecureString -AsPlainText -Force
+    $OU = "OU=" + $user.Afdeling + "," + $BasicOU
+    New-ADUser -Name $user.Naam -GivenName $user.Voornaam -Surname $user.Achternaam -Path $OU -AccountPassword $password -ChangePasswordAtLogon $True -Enabled $True
+}
